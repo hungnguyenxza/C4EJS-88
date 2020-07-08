b34bdf3ff6267c3ae8d2bccdc4e28a57daf9305c
Hùng Nguyễn <hungnx@pixelz.com>
2020-07-08 16:18:01 +0700

    session 2 & 3

---
+++ session3_datatype/datatype.js
@@ -0,0 +1,36 @@
+
+// let name = "Hung";
+// let school = 'ĐHBK';
+
+// // let welcome = "Xin chào " + name;
+// let welcome = `Xin chào ${name}, bạn học trường: ${school}`;
+// console.log(welcome);
+
+// let a = "1";
+// let b = "2";
+// console.log(a + b);
+// console.log(parseInt(a) + parseInt(b));
+// let a1 = parseInt(a);
+
+// let c = 3;
+// console.log(c);
+// console.log(a + c);
+
+// Cho người dùng nhập 2 số
+// Tính 4 phép tính + - * /: In ra màn hình kết quả
+
+let a = Number(prompt("Nhập số thứ nhất"));
+let b = Number(prompt("Nhập số thứ hai"));
+console.log(`Tổng ${a} + ${b} = ${a + b}`);
+console.log(`Hiệu ${a} - ${b} = ${a - b}`);
+console.log(`Tích ${a} * ${b} = ${a * b}`);
+console.log(`Thương ${a} / ${b} = ${a / b}`);
+
+
+
+
+
+
+
+
+
