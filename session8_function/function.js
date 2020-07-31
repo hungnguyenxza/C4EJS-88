6a2a4c5743f428730ea8b4bca6af3d36873f6533
Hùng Nguyễn <hungnx@pixelz.com>
2020-07-31 21:45:38 +0700

    function continue

--- session8_function/function.js
+++ session8_function/function.js
@@ -34,24 +34,137 @@ function welcome(s) {
 // }
 
 
-function tinhTong2(a, b){
-    let c = a + b;
-    return c;
-}
+// function tinhTong2(a, b){
+//     let c = a + b;
+//     return c;
+// }
 
-let sum = tinhTong2(10, 20);
-console.log(sum);
-console.log(`Trung bình cộng: ${sum / 2}`);
+// let sum = tinhTong2(10, 20);
+// console.log(sum);
+// console.log(`Trung bình cộng: ${sum / 2}`);
 
 // Nhận vào 1 số n, trả về tổng từ 1 -> n;
 // in ra kết quả (ngoài function)
 
-function tinhTong3(n){
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        sum += i;
+// Khai báo
+// function tinhTong3(n){
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+// let s = tinhTong3(100);
+// console.log(s);
+// console.log(tinhTong3(100));
+
+// function giaiThua(n){
+//     let p = 1;
+//     for (let i = 1; i <= n; i++) {
+//         p *= i;
+//     }
+//     return p;
+// }
+// // let p = giaiThua(5);
+// // console.log(p);//120
+
+// function tongCuaMang(arr){
+//     let s = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         s += arr[i];
+//     }
+//     return s;
+// }
+
+// function spliceMang(arr, n){
+//     let newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i] < n){
+//             newArr.push(arr[i]);
+//         }        
+//     }
+//     return newArr;
+// }
+
+// function gt(n){
+//     if(n === 1){
+//         return 1;
+//     }
+//     return n * gt(n - 1);
+// }
+
+// console.log(gt(10));
+
+
+
+
+// // Array Interation
+// let arr = [1, 20, 6, 12, 7, 10, 9, 5];
+
+// let index = arr.findIndex(function (v){
+//     return v === 100;
+// });
+
+// console.log(index);
+
+// arr.findIndex(function (v, i, a){
+
+// });
+
+// arr.findIndex(function (v, i){
+//     return i > 5;
+// })
+
+// function giaTriBang6(v, i, a){
+//     return v === 6;
+// }
+
+// arr.findIndex(giaTriBang6);
+
+// // Tìm giá trị = 20;
+// console.log(arr.find(giaTriLonHon20));
+// function giaTriLonHon20(v){
+//     return v > 20;
+// }
+// //
+// let phones = ['iPhone X',
+//             'Galaxy Note 10',
+//             'Xperia 2'];
+// //(không phân biệt hoa-thường)
+// // 1. Tìm tên 1 đt có chứa chữ 'a'
+// // 2. Tìm các đt có chứa chữ 'g'
+// // 3. Tìm các đt có tên dài hơn 8 kí tự
+// // 4. Tìm tên 1 đt có tên bắt đầu bởi 'x'
+
+let phones = [
+    {
+        'name': 'iPhone X',
+        'price': 20e6,
+        'brand': 'Apple',
+    },
+    {
+        'name': 'Galaxy Note 10',
+        'price': 22e6,
+        'brand': 'Samsung',
+    },
+    {
+        'name': 'Xperia 2',
+        'price': 15e6,
+        'brand': 'Sony',
     }
-    return sum;
-}
-let s = tinhTong3(100);
-console.log(s);
+];
+
+// 1. Tìm đt có tên chứa chứ 'iphone'
+// 2. Tìm đt có giá lớn hơn 18 triệu
+// 3. Tìm đt có giá trong khoảng 10 -> 20 triệu
+// 4. Có đt nào < 18 triệu không
+// 5. Tìm các đt của hãng Apple
+// 6. Tìm đt có giá cao nhất, nhỏ nhất
+// 7. Tính tổng giá trị của tất cả đt
+// 8. Có bao nhiêu đt của hãng Sony
+// 9. Sắp xếp đt theo tên a-z
+// 10. Sắp xếp đt theo giá cao -> thấp
+
+
+
+
